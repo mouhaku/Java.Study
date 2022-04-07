3e72711d90de51c702f586af1b600ddcbc04f18b
mouhaku <network20140325@gmail.com>
2022-04-07 18:28:26 +0900

    commit

---
+++ day01/src/io/github/mouhaku/day01/Test.java
@@ -0,0 +1,10 @@
+package io.github.mouhaku.day01;
+
+public class Test {
+    public static void main(String[] args) {
+        char ch =  '\u0065';
+        char cd = 'e';
+        System.out.println(ch);
+        System.out.println((int)cd);
+    }
+}

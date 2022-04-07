3918acda4900f56e538d0b5c77047b5ea56c3cd5
mouhaku <network20140325@gmail.com>
2022-04-07 14:33:16 +0900

    commit

--- day01/src/io/github/mouhaku/day01/Demo01.java
+++ day01/src/io/github/mouhaku/day01/Demo01.java
@@ -1,10 +1,26 @@
+/*
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/2/ITP1_2_D
+ */
 package io.github.mouhaku.day01;
+import java.util.Scanner;
 
 public class Demo01 {
     public static void main(String[] args)
     {
-        System.out.println("sdbfsdfbsdfbsdf");
-        System.out.println("sdbfsdfbsdfbsdf");
-        System.out.println("sdbfsdfbsdfbsdf");
+        Scanner sc = new Scanner(System.in);
+        int W = sc.nextInt();
+        int H = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int r = sc.nextInt();
+
+        if( (x + r) <= W && (x - r) >= 0 && (y + r) <= H && (y - r) >= 0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
     }
 }

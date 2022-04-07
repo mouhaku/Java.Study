6a2b317bd1abc5813cdd9238da49de5bc871b98c
mouhaku <network20140325@gmail.com>
2022-04-07 14:46:32 +0900

    commit

---
+++ day01/src/io/github/mouhaku/day01/Demo02.java
@@ -0,0 +1,13 @@
+package io.github.mouhaku.day01;
+
+public class Demo02
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0;i < 1000;i++)
+        {
+            System.out.println("Hello World");
+//            System.out.println(i);
+        }
+    }
+}

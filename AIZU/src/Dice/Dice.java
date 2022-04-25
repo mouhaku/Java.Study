802e8f3c4c0ca6fccc32373f4de6de545491a413
mengbo <103017119+mouhaku@users.noreply.github.com>
2022-04-25 11:32:17 +0900

    Merge remote-tracking branch 'origin/main'

---
+++ AIZU/src/Dice/Dice.java
@@ -0,0 +1,80 @@
+package Dice;
+
+public class Dice {
+    private int[] mynumber;
+    private char[] mydirection;
+
+    public void setMynumber(int[] mynumber) {
+        this.mynumber = mynumber;
+    }
+
+    public int[] getMynumber() {
+        return mynumber;
+    }
+
+    public void setMydirection(char[] mydirection) {
+        this.mydirection = mydirection;
+    }
+
+    public char[] getMydirection() {
+        return mydirection;
+    }
+
+    public int Dice(int[] num, char[] direction)
+    {
+        mynumber = num;
+        mydirection = direction;
+        for(int i = 0;i < mydirection.length;i++)
+        {
+            int[] tmp = new int[6];
+            switch(mydirection[i])
+            {
+                case 'E' :
+                {
+                    tmp[0] = mynumber[3];
+                    tmp[1] = mynumber[1];
+                    tmp[2] = mynumber[0];
+                    tmp[3] = mynumber[5];
+                    tmp[4] = mynumber[4];
+                    tmp[5] = mynumber[2];
+                    break;
+                }
+                case 'S' :
+                {
+                    tmp[0] = mynumber[4];
+                    tmp[1] = mynumber[0];
+                    tmp[2] = mynumber[2];
+                    tmp[3] = mynumber[3];
+                    tmp[4] = mynumber[5];
+                    tmp[5] = mynumber[1];
+                    break;
+                }
+                case 'W' :
+                {
+                    tmp[0] = mynumber[2];
+                    tmp[1] = mynumber[1];
+                    tmp[2] = mynumber[5];
+                    tmp[3] = mynumber[0];
+                    tmp[4] = mynumber[4];
+                    tmp[5] = mynumber[3];
+                    break;
+                }
+                case 'N' :
+                {
+                    tmp[0] = mynumber[1];
+                    tmp[1] = mynumber[5];
+                    tmp[2] = mynumber[2];
+                    tmp[3] = mynumber[3];
+                    tmp[4] = mynumber[0];
+                    tmp[5] = mynumber[4];
+                    break;
+                }
+            }
+            for(int j = 0;j < 6;j++)
+        {
+            mynumber[j] = tmp[j];
+        }
+        }
+        return mynumber[0];
+    }
+}
